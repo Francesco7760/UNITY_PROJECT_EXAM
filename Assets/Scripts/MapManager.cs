4e65f3cc489a3c3aaf76cdc44905652e337f2ea9
Freddy <cumalotrecords@gmail.com>
2022-06-24 19:05:15 +0200

    Maps Generation and Random Spawns

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    private GameObject roomsData;
+    private GameObject templates;
+    private Vector2[] positions= {new Vector2( 0, 0 )};
+    private int MapDimension = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomsData=Instantiate(Resources.Load("RoomsData", typeof(GameObject))) as GameObject;
+        roomsData.GetComponent<RoomsData>().Init(MapDimension,MapDimension);
+        roomsData.GetComponent<RoomsData>().setLevel(1);
+
+        Instantiate(Resources.Load("NESW",typeof(GameObject)),new Vector2(0,0),Quaternion.identity);
+        Instantiate(Resources.Load("Floor",typeof(GameObject)),new Vector2(0,0),Quaternion.identity);
+
+
+
+        //GameObject.FindGameObjectWithTag("RoomsData").GetComponent<RoomsData>().wait(5);
+        Invoke("ChangeRooms",3f);
+        Invoke("SpawnEn",3f);
+
+    }
+
+
+    void ChangeRooms()
+    {
+        this.roomsData.GetComponent<RoomsData>().ChangeRoom();
+    }
+    void SpawnEn()
+    {   
+        for (int x=0 ; x<roomsData.GetComponent<RoomsData>().positions.Count;x++ )
+        this.roomsData.GetComponent<RoomsData>().SpawnStuffs(roomsData.GetComponent<RoomsData>().positions[x],GameObject.FindGameObjectWithTag("Templates").GetComponent<Templates>().randomFloorTile,20);
+    }
+}
---
+++ Assets/Scripts/Templates.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Templates : MonoBehaviour
+{
+    public GameObject[] nordRooms;
+    public GameObject[] eastRooms;
+    public GameObject[] southRooms;
+    public GameObject[] westRooms;
+    public GameObject[] randomFloorTile;
+    public GameObject[] enemyArray;
+}
