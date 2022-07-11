916520de80c56ba79664d02e97f9b758ee2895ed
Antonio Ciavarrella <a.ciava97@gmail.com>
2022-07-11 20:29:51 +0200

    Loading & Fading

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+    /*public void StartGame()
     {
         SceneManager.LoadScene("ScenaMappa");
-    }
+    }*/
     public void ExitGame()
     {
         Application.Quit();
